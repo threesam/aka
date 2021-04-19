0b9244634edc76e3380b6132ad9c12ba857a1b2b
threesam <salvatoredangelo@protonmail.com>
2021-04-19 09:32:17 -0400

    styled Nav, changed blog to posts

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -8,16 +8,19 @@
 	a {
 		font-weight: 100;
 	}
+	span {
+		color: var(--primary);
+	}
 	.active {
 		font-weight: 900;
 	}
 </style>
 
 <header>
-	<a class="logo" aria-current="{segment === undefined ? 'page' : undefined}" href=".">threesam</a>
+	<a class="logo" aria-current="{segment === undefined ? 'page' : undefined}" href=".">Art<span>Killing</span>Apathy</a>
 
 	<nav>
-		<a rel="prefetch" class={segment === "blog" ? 'active' : ""} aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a>
+		<a rel="prefetch" class={segment === "posts" ? 'active' : ""} aria-current="{segment === 'posts' ? 'page' : undefined}" href="posts">posts</a>
 		<a rel="prefetch" class={segment === "about" ? 'active' : ""} aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a>
 		<a rel="prefetch" class={segment === "contact" ? 'active' : ""} aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">contact</a>
 	</nav>
