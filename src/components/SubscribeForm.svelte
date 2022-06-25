c9eeef9416b9e9a2dae26175c2420ffde577ca44
threesam <salvatoredangelo@protonmail.com>
2022-06-25 15:20:17 -0400

    test POST to endpoint

--- src/components/SubscribeForm.svelte
+++ src/components/SubscribeForm.svelte
@@ -1,8 +1,38 @@
+<script>
+	import { createForm } from 'svelte-forms-lib'
+
+	const { form, handleChange, handleSubmit } = createForm({
+		initialValues: {
+			email: '',
+		},
+		onSubmit: async values => {
+			const response = await fetch('/api/subscribe', {
+				method: 'POST',
+				body: JSON.stringify({ email: values }),
+			})
+
+			console.log(response)
+			const json = await response.json()
+			console.log(json)
+		},
+	})
+</script>
+
 <div class="subscribe-form">
 	<p>hey there, sign up for stuff or whatever</p>
-	<form action="/api/subscribe" method="post">
+	<form
+		action="/api/subscribe"
+		method="post"
+		on:submit|preventDefault={handleSubmit}
+	>
 		<label for="email">
-			<input type="email" name="email" id="email" />
+			<input
+				type="email"
+				name="email"
+				id="email"
+				on:change={handleChange}
+				bind:value={$form.email}
+			/>
 		</label>
 		<button type="submit">Subscribe</button>
 	</form>
