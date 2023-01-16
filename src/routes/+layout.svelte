64ac6cbeb08248fbcea8720bf1b6ec2efe1bd2ee
threesam <salvatoredangelo@protonmail.com>
2023-01-16 15:36:20 -0500

    updated global asset imports

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	import './normalize.css'
+	import './style.css'
+	import Nav from '$lib/components/Nav.svelte'
+	import Theme from '$lib/components/Theme.svelte'
+	import Footer from '$lib/components/Footer.svelte'
+</script>
+
+<Nav />
+
+<slot />
+
+<div>
+	<Theme />
+</div>
+
+<Footer />
+
+<style>
+	div {
+		position: fixed;
+		z-index: 10;
+		padding: 0.5rem;
+		padding-bottom: 0;
+		bottom: var(--containerPadding);
+		right: var(--containerPadding);
+		opacity: 0.8;
+	}
+</style>
