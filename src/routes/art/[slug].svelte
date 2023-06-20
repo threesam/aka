701952bfcbfbbbd4125346fbe5d40fc249316ab1
Sam <57677512+threesam@users.noreply.github.com>
2023-06-20 19:23:30 -0400

    uses sveltekit functions

--- src/routes/art/[slug].svelte
+++ src/routes/art/[slug].svelte
@@ -1,8 +1,8 @@
 <script context="module">
-	import client from '$lib/sanityClient'
+  import client from "$lib/sanityClient";
 
-	export async function load({ params: { slug } }) {
-		const query = /* groq */ `*[_type == "post" && slug.current == $slug][0]{
+  export async function load({ params: { slug } }) {
+    const query = /* groq */ `*[_type == "post" && slug.current == $slug][0]{
       title,
       cta,
       "image": featuredMedia.asset->url,
@@ -13,39 +13,39 @@
       publishedAt,
 			"author": authors[0]->name,
       "tags": tags[]->slug.current
-    }`
+    }`;
 
-		const post = await client.fetch(query, { slug })
+    const post = await client.fetch(query, { slug });
 
-		return {
-			props: {
-				post,
-			},
-		}
-	}
+    return {
+      props: {
+        post,
+      },
+    };
+  }
 </script>
 
 <script>
-	export const prerender = true
-	export let post
-	const { title, image, alt, body, publishedAt, author, cta, tags } = post
+  export const prerender = true;
+  export let post;
+  const { title, image, alt, body, publishedAt, author, cta, tags } = post;
 
-	import { format, parseISO } from 'date-fns'
-	import BlockContent from '@movingbrands/svelte-portable-text'
-	import serializers from '$lib/components/serializers'
-	import Hero from '$lib/components/Hero.svelte'
-	import Container from '$lib/components/Container.svelte'
-	import SEO from '$lib/components/SEO.svelte'
-	import Cta from '$lib/components/Cta.svelte'
+  import { format, parseISO } from "date-fns";
+  import BlockContent from "@movingbrands/svelte-portable-text";
+  import serializers from "$lib/components/serializers";
+  import Hero from "$lib/components/Hero.svelte";
+  import Container from "$lib/components/Container.svelte";
+  import SEO from "$lib/components/SEO.svelte";
+  import Cta from "$lib/components/Cta.svelte";
 
-	function isPoem(tags) {
-		if (tags && tags.includes('poem')) {
-			return 'content poem'
-		}
-		return 'content'
-	}
+  function isPoem(tags) {
+    if (tags && tags.includes("poem")) {
+      return "content poem";
+    }
+    return "content";
+  }
 
-	const containerClass = isPoem(tags)
+  const containerClass = isPoem(tags);
 </script>
 
 <SEO {...post} />
@@ -53,26 +53,26 @@
 <Hero data={{ title, image, alt }} />
 
 <Container>
-	<section class={containerClass}>
-		{#if author}
-			<p>
-				{format(parseISO(publishedAt), 'yyyy-MM-dd')}<br />by
-				<a href="/about">{author}</a>
-			</p>
-		{/if}
-		<BlockContent blocks={body} {serializers} />
-		{#if cta}
-			<Cta {...cta} />
-		{/if}
-	</section>
+  <section class={containerClass}>
+    {#if author}
+      <p>
+        {format(parseISO(publishedAt), "yyyy-MM-dd")}<br />by
+        <a href="/about">{author}</a>
+      </p>
+    {/if}
+    <BlockContent blocks={body} {serializers} />
+    {#if cta}
+      <Cta {...cta} />
+    {/if}
+  </section>
 </Container>
 
 <style>
-	p {
-		font-size: var(--smallText);
-	}
+  p {
+    font-size: var(--smallText);
+  }
 
-	section ul {
-		padding: 3rem;
-	}
+  section ul {
+    padding: 3rem;
+  }
 </style>
