ac3b8cbd065a84bd39389ef9041cedf5f6f00e09
Sam <57677512+threesam@users.noreply.github.com>
2024-03-06 21:34:40 -0500

    allows heros without images

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,73 +1,92 @@
 <script>
-  import { onMount } from "svelte";
-  import { scale } from "svelte/transition";
-  import SplashFilter from "./SplashFilter.svelte";
+    import { onMount } from "svelte";
+    import { scale } from "svelte/transition";
+    import SplashFilter from "./SplashFilter.svelte";
 
-  import { urlFor } from "$lib/utils/sanity";
+    import { urlFor } from "$lib/utils/sanity";
 
-  let w;
-  let h;
-  function parentWidth(node) {
-    w = node.parentElement.clientWidth;
-    h = node.parentElement.clientHeight;
-  }
+    let w;
+    let h;
+    function parentWidth(node) {
+        w = node.parentElement.clientWidth;
+        h = node.parentElement.clientHeight;
+    }
 
-  export let data;
-  const { image: src, alt, title } = data;
+    export let data;
+    const { image: src, alt, title } = data;
 
-  let show = false;
-  onMount(() => (show = true));
+    let show = false;
+    onMount(() => (show = true));
 </script>
 
-<section>
-  {#if show}
-    <div>
-      <h1 id={title}>{title}</h1>
-      <slot />
+{#if show && src}
+    <section>
+        <div>
+            <h1 id={title}>{title}</h1>
+            <slot />
+        </div>
+        <img
+            use:parentWidth
+            in:scale={{ duration: 2000, start: 1.2, opacity: 0.2 }}
+            src={urlFor(src)
+                .width(w)
+                .height(h)
+                .fit("crop")
+                .crop("entropy")
+                .auto("format")
+                .url()}
+            {alt}
+        />
+        <SplashFilter opacity="0.8" />
+    </section>
+{:else}
+    <div class="title-only">
+        <h1 id={title}>{title}</h1>
+        <slot />
     </div>
-    <img
-      use:parentWidth
-      in:scale={{ duration: 2000, start: 1.2, opacity: 0.2 }}
-      src={urlFor(src)
-        .width(w)
-        .height(h)
-        .fit("crop")
-        .crop("entropy")
-        .auto("format")
-        .url()}
-      {alt}
-    />
-    <SplashFilter opacity="0.8" />
-  {/if}
-</section>
+{/if}
 
 <style>
-  section {
-    position: relative;
-    height: 50vh;
-    width: 100%;
-    display: grid;
-    place-content: center;
-    text-align: center;
-    overflow: hidden;
-    padding: var(--containerPadding);
-    box-shadow: var(--level-1);
-  }
+    .title-only {
+        width: 100%;
+        padding-top: 6rem;
+        display: flex;
+        justify-content: center;
+    }
 
-  img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    z-index: -10;
-  }
+    .title-only h1 {
+        text-align: left;
+        width: 40rem;
+        padding: var(--containerPadding);
+        max-width: 100%;
+    }
 
-  h1 {
-    font-size: clamp(var(--h3), 5vw, var(--bigH));
-    max-width: 70rem;
-    margin: 0;
-    line-height: 1.1;
-  }
+    section {
+        position: relative;
+        height: 50vh;
+        width: 100%;
+        display: grid;
+        place-content: center;
+        text-align: center;
+        overflow: hidden;
+        padding: var(--containerPadding);
+        box-shadow: var(--level-1);
+    }
+
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        z-index: -10;
+    }
+
+    h1 {
+        font-size: clamp(var(--h3), 5vw, var(--bigH));
+        max-width: 70rem;
+        margin: 0;
+        line-height: 1.1;
+    }
 </style>
