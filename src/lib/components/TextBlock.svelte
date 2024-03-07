59f6f5b0b67cc0d1e70572818333663804c79a05
Sam <57677512+threesam@users.noreply.github.com>
2024-03-06 22:38:24 -0500

    pulls named import for portableText

--- src/lib/components/TextBlock.svelte
+++ src/lib/components/TextBlock.svelte
@@ -1,18 +1,18 @@
 <script>
-	export let text
-	import serializers from '$lib/components/serializers'
-	import PortableText from '@portabletext/svelte'
+  export let text;
+  import serializers from "$lib/components/serializers";
+  import { PortableText } from "@portabletext/svelte";
 </script>
 
 <section>
-	<slot name="before" />
-	<PortableText blocks={text} {serializers} />
-	<slot name="after" />
+  <slot name="before" />
+  <PortableText blocks={text} {serializers} />
+  <slot name="after" />
 </section>
 
 <style>
-	section {
-		max-width: 40rem;
-		margin: 0 auto;
-	}
+  section {
+    max-width: 40rem;
+    margin: 0 auto;
+  }
 </style>
