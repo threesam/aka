bb411a3826076bafc8987fa04bd87aae7b67eef3
Sam <57677512+threesam@users.noreply.github.com>
2024-03-06 22:35:09 -0500

    move prerender to layout

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -1,9 +1,7 @@
 <script>
-	export const prerender = true
-
-	import Container from '$lib/components/Container.svelte'
+  import Container from "$lib/components/Container.svelte";
 </script>
 
 <Container>
-	<slot />
+  <slot />
 </Container>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,38 +1,36 @@
 <script>
-	export const prerender = true
+  import { onMount } from "svelte";
+  import Contact from "$lib/components/Contact.svelte";
+  import SEO from "$lib/components/SEO.svelte";
 
-	import { onMount } from 'svelte'
-	import Contact from '$lib/components/Contact.svelte'
-	import SEO from '$lib/components/SEO.svelte'
-
-	// ssr p5 sketch
-	import sketch from '$lib/components/sketch.js'
-	let P5Sketch
-	onMount(async () => {
-		const mod = await import('$lib/components/CanvasP5.svelte')
-		P5Sketch = mod.default
-	})
+  // ssr p5 sketch
+  // import sketch from "$lib/components/sketch.js";
+  // let P5Sketch;
+  // onMount(async () => {
+  //   const mod = await import("$lib/components/CanvasP5.svelte");
+  //   P5Sketch = mod.default;
+  // });
 </script>
 
-<!-- <SEO title="Contact" description="Social links for Art Killing Apathy" /> -->
+<SEO title="Contact" description="Social links for Art Killing Apathy" />
 <svelte:head>
-	<title>Contact</title>
+  <title>Contact</title>
 </svelte:head>
 
 <main>
-	<section>
-		<Contact />
-		<!-- <svelte:component this={P5Sketch} {sketch} id=" contact-sketch" /> -->
-	</section>
+  <section>
+    <Contact />
+    <!-- <svelte:component this={P5Sketch} {sketch} id=" contact-sketch" /> -->
+  </section>
 </main>
 
 <style>
-	section {
-		display: grid;
-		place-content: center;
-		height: 100vh;
-		width: 100%;
-		position: relative;
-		overflow: hidden;
-	}
+  section {
+    display: grid;
+    place-content: center;
+    height: 100vh;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+  }
 </style>
